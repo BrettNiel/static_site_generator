2b5da195572a9618e64c23ea7032658d69cee8bc
Brett <bmniel13@gmail.com>
2026-06-16 15:33:02 -0400

    markdown_to_blocks function and test

--- src/test_utilities.py
+++ src/test_utilities.py
@@ -7,6 +7,7 @@ from utilities import extract_markdown_links
 from utilities import split_nodes_image
 from utilities import split_nodes_link
 from utilities import text_to_textnodes
+from utilities import markdown_to_blocks
 
 class TestUtilities(unittest.TestCase):
 
@@ -83,7 +84,8 @@ class TestUtilities(unittest.TestCase):
     def test_text_to_textnodes(self):
         node = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
         new_nodes = text_to_textnodes(node)
-        self.assertListEqual([
+        self.assertListEqual(
+            [
             TextNode("This is ", TextType.TEXT),
             TextNode("text", TextType.BOLD),
             TextNode(" with an ", TextType.TEXT),
@@ -94,7 +96,28 @@ class TestUtilities(unittest.TestCase):
             TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
             TextNode(" and a ", TextType.TEXT),
             TextNode("link", TextType.LINK, "https://boot.dev"),
-        ], new_nodes)
+            ], new_nodes,
+        )
+
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
 
     def test_extract_markdown_images(self):
         matches = extract_markdown_images("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)")
--- src/utilities.py
+++ src/utilities.py
@@ -71,6 +71,15 @@ def text_to_textnodes(text):
     list_text = split_nodes_link(list_text)
     return list_text
 
+def markdown_to_blocks(markdown):
+    split_lines = markdown.split('\n\n')
+    stripped_lines = []
+    for line in split_lines:
+        if not line:
+            continue
+        stripped_lines.append(line.strip())
+    return stripped_lines
+
 def extract_markdown_images(text):
     pattern = r'!\[([^\[\]]*)\]\(([^\(\)]*)\)'
     matches = re.findall(pattern, text)
